32c3e92b5c128664e322ecc7977a2a77d6fbc8eb
Runjin Zheng <runjin@merico.dev>
2022-05-25 18:46:15 +0800

    feat: add feishu okr models

---
+++ plugins/feishu/models/okr_user_objective.go
@@ -0,0 +1,56 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package models
+
+import (
+	"time"
+
+	"github.com/apache/incubator-devlake/models/common"
+)
+
+type FeishuOkrUserObjective struct {
+	ID                                 string   `json:"id" gorm:"type:varchar(255)"`
+	OkrID                              string   `json:"okr_id" gorm:"type:varchar(255)"`
+	MemberID                           string   `json:"member_id" gorm:"type:varchar(255)"`
+	Content                            string   `json:"content" gorm:"type:varchar(255)"`
+	Deadline                           string   `json:"deadline" gorm:"type:varchar(255)"`
+	Permission                         string   `json:"permission" gorm:"type:varchar(255)"`
+	MentionedUsers                     []string `json:"mentioned_user_list" gorm:"type:varchar(255)"`
+	ProgressRecords                    []string `json:"progress_record_list" gorm:"type:varchar(255)"`
+	AlignedObjectives                  []string `json:"aligned_objective_list" gorm:"type:varchar(255)"`
+	AligningObjectives                 []string `json:"aligning_objective_list" gorm:"type:varchar(255)"`
+	ProgressRateStatus                 string   `json:"progress_rate_status" gorm:"type:varchar(255)"`
+	ProgressRatePercent                int      `json:"progress_rate_percent"`
+	ProgressReport                     string   `json:"progress_report" gorm:"type:varchar(255)"`
+	ProgressRateStatusLastUpdatedTime  string   `json:"progress_rate_status_last_updated_time" gorm:"type:varchar(255)"`
+	ProgressRatePercentLastUpdatedTime string   `json:"progress_rate_percent_last_updated_time" gorm:"type:varchar(255)"`
+	ProgressRecordLastUpdatedTime      string   `json:"progress_record_last_updated_time" gorm:"type:varchar(255)"`
+	ProgressReportLastUpdatedTime      string   `json:"progress_report_last_updated_time" gorm:"type:varchar(255)"`
+	ScoreLastUpdatedTime               string   `json:"score_last_updated_time" gorm:"type:varchar(255)"`
+	Score                              int      `json:"score"`
+	Weight                             int      `json:"weight"`
+
+	StartTime time.Time
+
+	common.Model `json:"-"`
+	common.RawDataOrigin
+}
+
+func (FeishuOkrUserObjective) TableName() string {
+	return "_tool_feishu_okr_user_objective"
+}
